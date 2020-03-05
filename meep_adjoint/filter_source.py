da6498cb892aac2219030359cb177eab91354c77
Alec Hammond <ahammond36@gatech.edu>
2020-03-05 17:02:18 -0500

    fix simple filter

--- meep_adjoint/filter_source.py
+++ meep_adjoint/filter_source.py
@@ -4,10 +4,13 @@ from scipy import signal
 from meep import CustomSource
 import matplotlib.pyplot as plt
 from scipy.interpolate import Rbf, PchipInterpolator
+from scipy.special import erf
 
 
 class FilteredSource(CustomSource):
     def __init__(self,center_frequency,frequencies,frequency_response,dt,T,time_src,min_err=1e-6):
+        dt = dt/4
+        self.dt = dt
         self.center_frequency=center_frequency
         self.frequencies=frequencies
         self.time_src=time_src
@@ -15,60 +18,41 @@ class FilteredSource(CustomSource):
         f = self.func()
 
         # calculate dtft of input signal
-        signal_t = np.array([time_src.swigobj.current(t,dt) for t in np.arange(0,T,dt)])
-        signal_dtft = np.zeros((frequencies.shape),dtype=np.complex128)
-        for n, st in enumerate(signal_t):
-            for fi, f in enumerate(frequencies):
-                signal_dtft[fi] += np.exp(1j*2*np.pi*f*n*dt)*st
+        signal_t = np.array([time_src.swigobj.current(t,dt) for t in np.arange(0,T,dt)]) # time domain signal
+        signal_dtft = np.exp(1j*2*np.pi*frequencies[:,np.newaxis]*np.arange(0,signal_t.size)[np.newaxis,:]*dt)@signal_t # vectorize dtft for speed
 
         # multiply sampled dft of input signal with filter transfer function
-        H = signal_dtft #* frequency_response
+        H = signal_dtft * frequency_response
 
-        # fit final frequency response to rbf
-        H_rbf = Rbf(frequencies, H, function='gaussian')
-        f_rbf = np.arange(0,1/dt,1/T)
+        self.estimate_impulse_response(H)
 
-        # estimate impulse response of final frequency response using ifft
-        h = np.flipud(np.fft.ifft(H_rbf(f_rbf))) # flip signal becuase fft convention is backwards of meeps
-        t_h = np.arange(0,T,dt)
-
-        # fit impulse response to function to make implementation easy
-        h_rbf = PchipInterpolator(t_h, h)  # we don't need a ton of extra accuracy at this point -- just speed
-        #signal_fourier_transform = np.array([time_src.fourier_transform(f) for f in self.frequencies])
-        # view results
-        '''plt.figure()
-        plt.subplot(2,1,1)
-        plt.semilogy(t_h,np.abs(h))
-        #plt.plot(t_h,np.abs(h_rbf(t_h)),'--')
-        #plt.xlim(1/1.7,1/1.4)
-
-        plt.subplot(2,1,2)
-        plt.plot(t_h,np.unwrap(np.angle(h)))
-        #plt.plot(t_h,np.unwrap(np.angle(h_rbf(t_h))),'--')
-        #plt.xlim(1/1.7,1/1.4)
-        plt.show()
-        quit()'''
-
-        def f_temp(t):
-            return np.asscalar(h_rbf(t))
-
-        #self.estimate_impulse_response()
         # initialize super
-        super(FilteredSource, self).__init__(src_func=f_temp,center_frequency=self.center_frequency,is_integrated=False)
+        super(FilteredSource, self).__init__(src_func=f,center_frequency=self.center_frequency,is_integrated=False)
         
     def gaussian(self,f,f0,fwidth):
-        return np.exp(-(f-f0)**2/(2*fwidth**2))
-
+        return np.exp(-0.5*((f-f0)/fwidth)**2)
+    def antiderivative(self,f,n,f0,fwidth,T):
+        a = np.sqrt(np.pi/2)*fwidth
+        phase = np.exp(-2*np.pi*n*T*(np.pi*n*T*fwidth*fwidth + 1j*f0))
+        kernel = erf(f/(np.sqrt(2)*fwidth)-f0/(np.sqrt(2)*fwidth)+1j*np.sqrt(2)*np.pi*n*fwidth*T)
+        return a*phase*kernel
+    def dtft_gaussian(self,n,f0,fwidth,T):
+        f_start = 0
+        f_end = 1/T
+        return T*(self.antiderivative(f_end,n,f0,fwidth,T) - self.antiderivative(f_start,n,f0,fwidth,T))
     def __call__(self,t):
+        n = int(np.round(t/self.dt))
+        #print(t/self.dt,n)
+        vec = self.dtft_gaussian(n,self.frequencies,self.gaus_widths,self.dt)
         # simple RBF with gaussian kernel reduces to inner product at time step
-        return np.dot(self.gauss_t(t,self.frequencies,self.gaus_widths),self.nodes)
+        return np.dot(vec,self.nodes)
     
     def func(self):
         def _f(t): 
             return self(t)
         return _f
     
-    def estimate_impulse_response(self):
+    def estimate_impulse_response(self,H):
         '''
         find gaussian weighting coefficients.
 
@@ -76,16 +60,11 @@ class FilteredSource(CustomSource):
         '''
         # Use vandermonde matrix to calculate weights of each gaussian.
         # Each gaussian is centered at each frequency point
-        h = self.frequency_response
         def rbf_l2(fwidth):
-            vandermonde = np.zeros((self.frequencies.size,self.frequencies.size),dtype=np.complex128)
-            for ri, rf in enumerate(self.frequencies):
-                for ci, cf in enumerate(self.frequencies):
-                    vandermonde[ri,ci] = self.gauss_f(rf,cf,fwidth)
-            
-            nodes = np.matmul(np.linalg.pinv(vandermonde),h)
-            h_hat = np.matmul(vandermonde,nodes)
-            l2_err = np.sum(np.abs(h-h_hat)**2)
+            vandermonde = self.gaussian(self.frequencies[:,np.newaxis],self.frequencies[np.newaxis,:],fwidth)
+            nodes = np.matmul(np.linalg.pinv(vandermonde),H)
+            H_hat = np.matmul(vandermonde,nodes)
+            l2_err = np.sum(np.abs(H-H_hat)**2)
             return nodes, l2_err
         
         df = self.frequencies[2] - self.frequencies[1]
@@ -99,37 +78,22 @@ class FilteredSource(CustomSource):
                 err_high = False
             else:
                 fwidth = 0.5 * fwidth
+            print(l2_err)
         self.gaus_widths = fwidth
         self.nodes = nodes
 
         from matplotlib import pyplot as plt
 
-        temp = self.gauss_f(self.frequencies[:,np.newaxis],self.frequencies,fwidth)
+        temp = self.gaussian(self.frequencies[:,np.newaxis],self.frequencies,fwidth)
         i_hat = np.inner(self.nodes,temp)
 
         plt.figure()
         plt.subplot(2,1,1)
-        plt.semilogy(self.frequencies,np.abs(h)**2)
+        plt.semilogy(self.frequencies,np.abs(H)**2)
         plt.semilogy(self.frequencies,np.abs(i_hat)**2)
 
         plt.subplot(2,1,2)
-        plt.plot(self.frequencies,np.unwrap(np.angle(self.frequency_response)))
+        plt.plot(self.frequencies,np.unwrap(np.angle(H)))
         plt.plot(self.frequencies,np.unwrap(np.angle(i_hat)))
-    
-    def gauss_t(self,t,f0,fwidth):
-        s = 5
-        w = 1.0 / fwidth
-        t0 = w * s
-        tt = (t - t0)
-        amp = 1.0 / (-1j*2*np.pi*f0)
-        return amp * np.exp(-tt * tt / (2 * w * w))*np.exp(-1j*2 * np.pi * f0 * tt) #/ np.sqrt(2*np.pi)
-    
-    def gauss_f(self,f,f0,fwidth):
-        s = 5
-        w = 1.0 / fwidth
-        t0 = w * s
-        omega = 2.0 * np.pi * f
-        omega0 = 2.0 * np.pi * f0
-        delta = (omega - omega0) * w
-        amp = self.center_frequency/f0#/np.sqrt(omega0)#1j / (omega0)
-        return amp * w * np.exp(1j*omega*t0) * np.exp(-0.5 * delta * delta)
+        '''plt.show()
+        quit()'''
